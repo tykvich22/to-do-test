1027b9accd62745112ffa6a4272e0d32bf3e00f1
tykvich22 <poketyti4@gmail.com>
2024-10-08 16:44:15 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 export default defineConfig({
+  base: '/to-do-test',
   plugins: [
     vue({
       template: { transformAssetUrls },
